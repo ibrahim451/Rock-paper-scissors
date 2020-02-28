7b2d3688eb261cd47e082026d4e1d0312b0d4cdb
unknown <ibrahimmustafa451@gmai.com>
2020-02-27 20:36:29 -0800

    added the decidewinner and playgame

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PlayerCard from './PlayerCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Game extends Component {
+  constructor() {
+    super();
+    this.signs = ["rock", "scissors", "paper"];
+    this.state = {
+      playerOne: "rock",
+      playerTwo: "scissors",
+    }
+
+  }
+
+
+  //this is where methods are put
+  // '=>' that binds it and does the same function as 'super()'
+  playGame = () => {
+    this.setState({
+      playerOne: this.signs[Math.floor(Math.random()*3)],
+      playerTwo: this.signs[Math.floor(Math.random()*3)]
+    })
+
+  }
+
+  decideWinner = () => {
+    
+  //  if (this.PlayerOne === this.playerTwo) {
+//    return "Its a draw";   } else
+ if (this.state.playerOne === "rock" && this.state.playerTwo === "scissors") {
+    return "Player 1 wins!"
+  } else if (this.state.playerOne === "rock" && this.state.playerTwo === "paper") {
+    return "Player 2 wins!"
+  }else if (this.state.playerOne === "paper" && this.state.playerTwo === "scissors") {
+    return "Player 2 wins!"
+  }else if (this.state.playerOne === "paper" && this.state.playerTwo === "rock") {
+    return "Player 1 wins!"
+  }else if (this.state.playerOne === "scissors" && this.state.playerTwo === "rock") {
+    return "Player 2 wins!"
+  }else if (this.state.playerOne === "scissors" && this.state.playerTwo === "paper") {
+    return "Player 2 wins!"
+  } else
+    return "Draw! -____-  "
+
 }
 
-export default App;
+
+
+
+
+  render() {
+    return(
+      <div className="container">
+        <div>
+        <PlayerCard sign={this.state.playerOne}/>
+        <PlayerCard sign={this.state.playerTwo}/>
+        </div>
+        <div className= "winner">{this.decideWinner()}</div>
+        <button type="button" onClick={this.playGame}> Play the Game </button>
+      </div>
+    )
+  }
+};
+
+export default Game;
